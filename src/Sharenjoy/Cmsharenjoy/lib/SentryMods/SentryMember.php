6e70e695793f8d8388deb5f65f20863e5d7ad1f2
Ronald Jian <ronald.jian@gmail.com>
2014-05-22 03:38:03 +0800

    modify some files

--- src/Sharenjoy/Cmsharenjoy/lib/SentryMods/SentryMember.php
+++
@@ -1,8 +0,0 @@
-<?php namespace Sharenjoy\Cmsharenjoy\Lib\SentryMods;
-
-use Illuminate\Support\Facades\Facade;
-
-class SentryMember extends Facade
-{
-    protected static function getFacadeAccessor(){ return 'sentry.member'; }
-}
