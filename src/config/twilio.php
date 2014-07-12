772dba7f134de17df4ea72924b750c387198dbc7
Ronald Jian <ronald.jian@gmail.com>
2014-07-12 17:22:45 +0800

    modify some files

--- src/config/twilio.php
+++ src/config/twilio.php
@@ -2,9 +2,9 @@
 
 return array(
 
-    'from'       => '16019752648',
-    'account_id' => 'ACcfb303eb79ad84b74f2682ca6c1b477c',
-    'auth_token' => '09a7ce2c04cae5590963ee29102fb16e',
+    'from'       => '',
+    'account_id' => '',
+    'auth_token' => '',
     
     // test
     // 'from'       => '15005550000',
