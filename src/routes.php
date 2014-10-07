98e003ee193a2cc8d90846e18d1b8abba4434be8
Ronald Jian <ronald.jian@gmail.com>
2014-10-07 19:37:08 +0800

    modify route.php

--- src/routes.php
+++ src/routes.php
@@ -20,16 +20,10 @@ Route::group(['prefix' => $urlSegment], function()
 {
     Route::controller('test'    , 'Sharenjoy\Cmsharenjoy\Controllers\TestableController');
 
+    Route::controller('tag'     , 'Sharenjoy\Cmsharenjoy\Repo\Tag\TagController');
     Route::controller('user'    , 'Sharenjoy\Cmsharenjoy\User\UserController');
-    Route::controller('member'  , 'Sharenjoy\Cmsharenjoy\Repo\Member\MemberController');
-    Route::controller('order'   , 'Sharenjoy\Cmsharenjoy\Repo\Order\OrderController');
     Route::controller('filer'   , 'Sharenjoy\Cmsharenjoy\Filer\FilerController');
     Route::controller('setting' , 'Sharenjoy\Cmsharenjoy\Setting\SettingController');
-    Route::controller('product' , 'Sharenjoy\Cmsharenjoy\Repo\Product\ProductController');
-    Route::controller('qna'     , 'Sharenjoy\Cmsharenjoy\Repo\Qna\QnaController');
-    Route::controller('report'  , 'Sharenjoy\Cmsharenjoy\Repo\Report\ReportController');
-    Route::controller('tag'     , 'Sharenjoy\Cmsharenjoy\Repo\Tag\TagController');
-    Route::controller('post'    , 'Sharenjoy\Cmsharenjoy\Repo\Post\PostController');
     Route::controller('category', 'Sharenjoy\Cmsharenjoy\Repo\Category\CategoryController');
     // Route::controller(''        , 'Sharenjoy\Cmsharenjoy\Controllers\DashController');
 });
