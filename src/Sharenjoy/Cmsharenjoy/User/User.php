823a096134a1509dc9d413156bb2dde531ac8aba
Ronald Jian <ronald.jian@gmail.com>
2014-11-27 04:01:00 +0800

    add the ConfigTrait of User

--- src/Sharenjoy/Cmsharenjoy/User/User.php
+++ src/Sharenjoy/Cmsharenjoy/User/User.php
@@ -7,6 +7,7 @@ use Sharenjoy\Cmsharenjoy\Core\Traits\CommonModelTrait;
 
 class User extends SentryUserModel implements LaravelUserInterface, RemindableInterface {
 
+    use ConfigTrait;
     use CommonModelTrait;
 
     protected $table  = 'users';
@@ -19,24 +20,6 @@ class User extends SentryUserModel implements LaravelUserInterface, RemindableIn
         'avatar',
         'description'
     ];
-
-    protected $eventItem = [];
-
-    public $formConfig = [
-        'avatar'                => ['order' => '5', 'help'=>'建議尺寸180x180px', 'size'=>'180x180'],
-        'name'                  => ['order' => '10'],
-        'email'                 => ['order' => '20'],
-        'phone'                 => ['order' => '30'],
-        'password'              => ['order' => '40'],
-        'password_confirmation' => ['order' => '50'],
-        'description'           => ['order' => '60'],
-    ];
-
-    public $createFormConfig = [];
-    public $updateFormConfig = [];
-
-    public $createFormDeny   = [];
-    public $updateFormDeny   = ['password', 'password_confirmation'];
 
     protected $hidden = ['password'];
 
