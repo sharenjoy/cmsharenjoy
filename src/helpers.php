fd5e4cadce49365d028774f505c6f9284f711563
Ronald Jian <ronald.jian@gmail.com>
2015-03-04 13:57:27 +0800

    add unique_array to helper

--- src/helpers.php
+++ src/helpers.php
@@ -484,3 +484,33 @@ if ( ! function_exists('error'))
         return message()->error($message);
     }
 }
+
+if ( ! function_exists('unique_array'))
+{
+    /**
+     * To filter the array for the unique by the key
+     * Input
+     * [0]=>array("id"=>"1","name"=>"Mike","num"=>"9876543210"),
+     * [1]=>array("id"=>"2","name"=>"Carissa","num"=>"08548596258"),
+     * [2]=>array("id"=>"1","name"=>"Mathew","num"=>"784581254"),
+     * Output
+     * [0]=>array("id"=>"1","name"=>"Mike","num"=>"9876543210"),
+     * [1]=>array("id"=>"2","name"=>"Carissa","num"=>"08548596258"),
+     */
+    function unique_array($array, $key)
+    {
+        $temp_array = array();
+        $i = 0;
+        $key_array = array();
+        
+        foreach($array as $val){
+            if( ! in_array($val[$key],$key_array)){
+                $key_array[$i] = $val[$key];
+                $temp_array[$i] = $val;
+            }
+            $i++;
+        }
+        return $temp_array;
+    }
+}
+
