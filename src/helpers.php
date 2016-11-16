957b9248dae4dbcec221d962796d5de0966b9753
Ronald <ronald.jian@gmail.com>
2016-11-16 08:50:22 +0800

    add language function

--- src/helpers.php
+++ src/helpers.php
@@ -132,6 +132,30 @@ if ( ! function_exists('current_backend_language'))
     }
 }
 
+if ( ! function_exists('current_language'))
+{
+    function current_language()
+    {
+        $whichEnd = session()->get('sharenjoy.whichEnd');
+        
+        if ($whichEnd == 'backEnd') {
+            return current_backend_language();
+        } elseif ($whichEnd == 'frontEnd') {
+            if (config('cmsharenjoy.language_default')) {
+                $language = config('cmsharenjoy.language_default');
+
+                if (array_key_exists(strtolower(Request::segment(1)), config('cmsharenjoy.language'))) {
+                    $language = strtolower(Request::segment(1));
+                }
+
+                return $language;
+            }
+        }
+
+        return null;
+    }
+}
+
 if ( ! function_exists('showActivated'))
 {
 	function showActivated()
